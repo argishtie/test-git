be3463217449c81017606b8baa2fa7f45b9bf90f
Argishti Egoryan <ayegoryan@ghost-services.com>
2025-02-12 20:58:40 +0400

    Added new changes in app.js and added new file test.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log(1);
+import 'dotenv/config';
+
+import http from "http";
+
+const port = 3000;
+const hostname = '127.0.0.1';
+
+const server = http.createServer(async (req, res) => {
+  try {
+    res.statusCode = 200;
+    res.end("Hello world!");
+  } catch (e) {
+    res.statusCode = 404;
+    res.end(e.message);
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log("Server running:", port);
+});
+
